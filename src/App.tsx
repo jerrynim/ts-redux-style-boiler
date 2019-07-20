5933f56f2dbaa1a8a1d80bf613213146e298eb68
jerrynim <tjerry3@naver.com>
2019-07-20 23:35:42 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 const App: React.FC = () => {
+  const [text, setText] = useState("");
+  const count = useSelector((state: any) => state.counter);
+  const dispatch = useDispatch();
+  const increase = useCallback(() => dispatch({ type: "counter/INCREMENT" }), [
+    dispatch
+  ]);
+  const onInputChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setText(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => increase()}>asdf </button>
+      <button onClick={() => dispatch({ type: "counter/DECREMENT" })}>
+        increase
+      </button>
+      <button
+        onClick={() => dispatch({ type: "counter/SETTEXT", payload: text })}
+      >
+        change
+      </button>
+      <br />
+      {count.count}
+      {count.text}
+      <input type="text" value={text} onChange={onInputChange} />
     </div>
   );
-}
+};
 
 export default App;
